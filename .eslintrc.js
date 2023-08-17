cadb451d4de39e491f55da881bb5b441b3232f9c
Yancong Yin <60558366+agathayin@users.noreply.github.com>
2023-08-17 14:38:36 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,37 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ["airbnb", "airbnb/hooks", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  overrides: [
+    {
+      files: [".eslintrc.{js,cjs}", "server/modules/**/**/*.js"],
+      env: {
+        node: true,
+        browser: false,
+      },
+      parserOptions: {
+        sourceType: "script",
+      },
+      rules: {
+        "no-unused-vars": "warn",
+      },
+    },
+  ],
   parserOptions: {
+    ecmaVersion: "latest",
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
   },
-  plugins: ["react"],
   rules: {
     "react/react-in-jsx-scope": "off",
+    "no-useless-escape": "off",
+    "no-empty": "off",
   },
   ignorePatterns: ["client/build/*"],
 };
