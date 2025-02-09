ed482fe7764b23d31c726b0cb65759e24764bb96
Yancong Yin <60558366+agathayin@users.noreply.github.com>
2025-02-09 15:39:29 -0500

    listing socket sample

--- server/modules/amzListing/controllers/listing.js
+++ server/modules/amzListing/controllers/listing.js
@@ -30,3 +30,28 @@ exports.list = async function (req, res) {
   const data = await Amzlisting.find({});
   return res.jsonp(data);
 };
+
+exports.syncItem = async function (req, res) {
+  try {
+    if (!req.query.sku) {
+      return res.status(400).send({ message: "Missing SKU" });
+    }
+    let updated = { updated: true };
+    if (updated && !updated.message) {
+      try {
+        var io = req.app.get("socketio");
+        io.emit("amzItemChange", {
+          data: req.query.sku,
+          message: req.query.sku + " is changed. Please refresh to get latest data.",
+          created: Date.now(),
+          username: req.user.displayName,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    res.jsonp(updated);
+  } catch (err) {
+    console.log("Failed to sync for " + req.query.sku, err.message);
+  }
+};
